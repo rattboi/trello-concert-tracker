ad8b3decacf0b4253d21563187dacc0988734be7
Bradon Kanyid <bradon@kanyid.org>
2017-07-07 17:03:27 -0700

    Add a few cleanups, including demophoon's comments
    - Use Arrow's tz replace based on string. No need to import tzinfo then.
    - Combine the string replacements to cleanup artists
    - Just use list comprehension instead of map. Same crap anyways.

--- trello/plugins/analogtheater.py
+++ trello/plugins/analogtheater.py
@@ -5,25 +5,20 @@ from __future__ import print_function
 
 import re
 import json
-import string
 import arrow
-
 import requests
 from trello.plugins.sync_to_trello import sync_to_trello
-from dateutil import tz
 
 
 def clean_artists(artists):
     final_artists = []
     for artist in artists:
         artist = re.sub(r'\([^)]*\)', '', artist)
-        artist = artist.replace('w/', ',')
-        artist = artist.replace('/', ',')
-        artist = artist.replace('&', ',')
-        artist = artist.replace('featuring', ',')
+        for sym in ['w/', '/', '&', 'featuring']:
+            artist = artist.replace(sym, ',')
         artist = ','.join(artist.rsplit(' and ', 1))
         artist = artist.split(',')
-        artist = map(string.strip, artist)
+        artist = [x.strip() for x in artist]
         artist = [x for x in artist
                   if (x.lower() != u'guests') and (x)]
         final_artists.extend(artist)
@@ -31,7 +26,7 @@ def clean_artists(artists):
     return final_artists
 
 
-def parse_event(event, default_venue=None):
+def parse_event(event):
     artists = event['performing']
     if not artists:
         artists = [event['title']]
@@ -52,9 +47,9 @@ def parse_event(event, default_venue=None):
 
     date_str = event['starts_at']
     time_str = event['doors_at']
-
     date = arrow.get("{} {}".format(date_str, time_str),
-                     "YYYY-MM-DD HH:mm").replace(tzinfo=tz.tzlocal())
+                     "YYYY-MM-DD HH:mm").replace(tzinfo='local')
+
     fobj = {
         'headliners': headliners,
         'openers': openers,
@@ -86,10 +81,9 @@ def main(trello, secrets):
 
         final_events = []
         for event in events:
-            parsed_event = parse_event(event, venue)
+            parsed_event = parse_event(event)
             final_events.append(parsed_event)
         print("Found {} items.".format(len(final_events)))
-
         sync_to_trello(trello, secrets, final_events)
 
 
