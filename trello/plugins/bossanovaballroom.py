6f613aaf8aaaab80d044a0e9e29a716163aa3eac
Bradon Kanyid <bradon@kanyid.org>
2017-07-11 14:39:02 -0700

    Pull most fields from embedded json. Cleanup.

--- trello/plugins/bossanovaballroom.py
+++ trello/plugins/bossanovaballroom.py
@@ -5,9 +5,9 @@ from __future__ import print_function
 
 import arrow
 import requests
+import json
 from bs4 import BeautifulSoup as bs
 from trello.plugins.sync_to_trello import sync_to_trello
-from dateutil import tz
 
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
@@ -27,27 +27,6 @@ def clean_artists(artists):
     return [a.strip() for a in artists.split(',') if a.strip().lower() != 'guests']
 
 
-def get_date(doc):
-    date_elem = doc.find(class_='tribe-events-start-date')
-    date_str = date_elem['title']
-    time_elem = doc.find(class_='tribe-events-start-time')
-    time_str = time_elem.text.split('-')[0].strip()
-    full_str = "{} {}".format(date_str, time_str)
-
-    date = arrow.get(full_str, "YYYY-MM-DD h:mm a").replace(tzinfo=tz.tzlocal())
-
-    return date
-
-
-def get_description(doc):
-    description = None
-    has_description = doc.find(class_='tribe-events-single-event-description')
-    if has_description:
-        has_description.find(class_='social-sharing').decompose()
-        description = has_description.text
-    return description
-
-
 def get_ticket(doc):
     ticket_link = None
     has_ticket_link = doc.find(class_='tribe-events-event-url')
@@ -58,13 +37,6 @@ def get_ticket(doc):
         if sold_out:
             ticket_link = "(Sold Out)"
     return ticket_link
-
-def get_venue(doc):
-    venue = None
-    has_venue = doc.find(class_='tribe-venue')
-    if has_venue:
-        venue = has_venue.text.strip()
-    return venue
 
 
 def get_restriction(description):
@@ -78,11 +50,16 @@ def get_restriction(description):
 def get_rest(url):
     content = requests.get(url, headers={"User-Agent": ua})
     doc = bs(content.text, 'html.parser')
-
-    date = get_date(doc)
-    description = get_description(doc)
     ticket = get_ticket(doc)
-    venue = get_venue(doc)
+
+    # Get the embedded json blob. Having things structured for us is a win!
+    data = [x for x in doc.find_all('script')
+            if x.get('type') == u'application/ld+json'][0].text
+    jdata = json.loads(data)[0]
+
+    date = arrow.get(jdata['startDate']).to('local')
+    description = jdata['description']
+    venue = jdata['location']['name']
     age_restriction = get_restriction(description)
 
     return (date, description, ticket, venue, age_restriction)
@@ -97,9 +74,6 @@ def parse_event(event, default_venue=None):
     headliners, openers = [artists[0]], artists[1:]
 
     (date, description, ticket_link, venue, age_restriction) = get_rest(entry['href'])
-
-    if venue is None:
-        venue = default_venue
 
     fobj = {
         'headliners': headliners,
@@ -132,7 +106,6 @@ def main(trello, secrets):
         final_events = []
         # deal with pagination summary page
         while True:
-            print("Fetching page {}".format(index))
             content = requests.get(url.format(index),
                                    headers={"User-Agent": ua})
             doc = bs(content.text, 'html.parser')
